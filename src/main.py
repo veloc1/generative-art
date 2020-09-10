5e19136ea01ae23baa6db20ac4789ea0c30ae1d3
Pavel <useless.dinosaur@veloc1.me>
2020-09-10 23:45:32 +0300

    More advanced pyglet

--- src/main.py
+++ src/main.py
@@ -1,22 +1,32 @@
 import pyglet
+from pyglet import shapes
+from pyglet.window import mouse, key
 
-window = pyglet.window.Window()
 
-label = pyglet.text.Label(
-    "Hello, world",
-    font_name="Times New Roman",
-    font_size=36,
-    x=window.width // 2,
-    y=window.height // 2,
-    anchor_x="center",
-    anchor_y="center",
-)
+window = pyglet.window.Window(caption="Wanna see some art?")
+batch = pyglet.graphics.Batch()
+
+
+line2 = shapes.Line(150, 150, 444, 111, width=4, color=(200, 20, 20), batch=batch)
 
 
 @window.event
 def on_draw():
     window.clear()
-    label.draw()
+    batch.draw()
+
+
+@window.event
+def on_key_press(symbol, modifiers):
+    print("A key was pressed")
+    if symbol == key.A:
+        print('The "A" key was pressed.')
+
+
+@window.event
+def on_mouse_press(x, y, button, modifiers):
+    if button == mouse.LEFT:
+        print("The left mouse button was pressed.")
 
 
 pyglet.app.run()
